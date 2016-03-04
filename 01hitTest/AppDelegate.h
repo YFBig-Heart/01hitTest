7b7d8036d0cc525027dbbb7e2e141334d2c8f8fc
HM <hm@hmdeimac.local>
2016-03-04 08:30:37 +0800

    Initial Commit

---
+++ 01hitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  01hitTest
+//
+//  Created by HM on 16/3/4.
+//  Copyright © 2016年 HM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 01hitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  01hitTest
+//
+//  Created by HM on 16/3/4.
+//  Copyright © 2016年 HM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
